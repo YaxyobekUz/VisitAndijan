e9fa8ba72175409568ec14415e19bfaf828ce561
mryaxyobek <pubgn9642@gmail.com>
2023-06-16 17:42:00 +0500

    Added header js codes

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ elLangBtn.addEventListener('click', function () {
     elArrowRotate.classList.toggle('rotate0deg');
     elArrowRotate.classList.toggle('rotate180deg');
 });
+
+// travel list 
+const elTravelListBtn = document.querySelector('.travel-list-btn');
+const elTravelList = document.querySelector('.travel-list');
+const elTravelBtnArrowRotate = document.querySelector('.travel-list-arrow-icon');
+
+elTravelListBtn.addEventListener('click', function () {
+    elTravelList.classList.toggle('bring-down');
+    elTravelList.classList.toggle('display-none');
+    elTravelBtnArrowRotate.classList.toggle('rotate0deg');
+    elTravelBtnArrowRotate.classList.toggle('rotate180deg');
+});
+
+// night and light mode 
+const elNightModeBtn = document.querySelector('.night-mode-btn');
+
+elNightModeBtn.addEventListener('click', function () {
+    document.body.classList.toggle('night-mode');
+})
