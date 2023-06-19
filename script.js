3cbee8d662ac00499b05f1b909231def18d55cbd
mryaxyobek <pubgn9642@gmail.com>
2023-06-19 15:54:28 +0500

    Added about section html codes

--- script.js
+++ script.js
@@ -30,15 +30,15 @@ function addClass(element, className) {
   });
 
 // lang list 
-const elLangBtn = document.querySelector('.lang-btn');
-const elLangListSecondChild = document.querySelector('.lang-list-last-child');
-const elArrowRotate = document.querySelector('.lang-list-arrow-icon');
+// const elLangBtn = document.querySelector('.lang-btn');
+// const elLangListSecondChild = document.querySelector('.lang-list-last-child');
+// const elArrowRotate = document.querySelector('.lang-list-arrow-icon');
 
-elLangBtn.addEventListener('click', function () {
-    elLangListSecondChild.classList.toggle('bring-down');
-    elArrowRotate.classList.toggle('rotate0deg');
-    elArrowRotate.classList.toggle('rotate180deg');
-});
+// elLangBtn.addEventListener('click', function () {
+//     elLangListSecondChild.classList.toggle('bring-down');
+//     elArrowRotate.classList.toggle('rotate0deg');
+//     elArrowRotate.classList.toggle('rotate180deg');
+// });
 
 // travel list 
 const elTravelListBtn = document.querySelector('.travel-list-btn');
